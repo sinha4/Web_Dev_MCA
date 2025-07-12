6b17d1edef999ea8e7958cc5606ea75ba05ca5af
sinha4 <shambhavisinha404@gmail.com>
2025-07-12 11:35:46 +0530

    Travel Site
    awaitingg a lot of updates

---
+++ project/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bulma/css/bulma.min.css';
+import Card from './card';
+
+
+function App() {
+  return (
+    <div className="App">
+      <section class="hero is-primary">
+  <div class="hero-body">
+    <p class="title">Personal Digital Assistant</p>
+  </div>
+</section>
+<card
+imag />
+<div class="container is-max-desktop">
+  <div class="notification is-primary">
+    This container has a <code>max-width</code> of
+    <code>$desktop - $container-offset</code> on widescreen and fullhd.
+  </div>
+</div>
+    </div>
+  );
+}
+
+export default App;
